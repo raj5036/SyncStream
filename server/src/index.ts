d99f09a84dbe62639925c039d8f6fbe7fc532fe4
Raj Karmakar <97rajmath@gmail.com>
2025-11-13 11:46:02 +0530

    1. Add feature to play video by Video-URL. 2. Add feature to seek forward/backward.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,5 @@
+import "dotenv/config";
+
 import express, { Request, Response } from "express";
 import cors from "cors";
 import http from "http";
@@ -6,7 +8,7 @@ import { Server } from "socket.io";
 import { PlaybackState } from "./types/playback.types";
 
 const app = express();
-const PORT = process.env.PORT || "8080";
+const PORT = process.env.PORT || "5000";
 
 app.use(express.json());
 app.use(cors());
