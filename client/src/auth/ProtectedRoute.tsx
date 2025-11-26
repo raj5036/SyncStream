ef477250a706a4db628b9650635cde454aea5ca7
Raj Karmakar <97rajmath@gmail.com>
2025-11-26 21:07:17 +0530

    style: Add HTML template for Login page

--- client/src/auth/ProtectedRoute.tsx
+++ client/src/auth/ProtectedRoute.tsx
@@ -2,6 +2,7 @@ import type React from "react";
 import { CommonUtils } from "../utils/CommonUtils";
 import { LOCAL_STORAGE_KEYS } from "../utils/Constants";
 import { Navigate } from "react-router-dom";
+import { AppRoutes } from "../routes/AppRoutes";
 
 const isAuthenticated = (): boolean => {
 	return !!CommonUtils.getItemFromLocalStorage(LOCAL_STORAGE_KEYS.USER_TOKEN);
@@ -11,7 +12,7 @@ export default function ProtectedRoute({ children }: {
 	children: React.ReactNode
 }) {
 	if (!isAuthenticated()) {
-		return <Navigate to="/login" replace />;
+		return <Navigate to={AppRoutes.AUTH.LOGIN} replace />;
 	}
 	return children;
 };
--- client/src/pages/LoginPage/LoginPage.tsx
+++ client/src/pages/LoginPage/LoginPage.tsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import { useState } from "react";
+import "./LoginPage.css";
+import GoogleIcon from "../../assets/svgs/GoogleIcon.svg";
+import FacebookIcon from "../../assets/svgs/FacebookIcon.svg"
 
 const LoginPage = () => {
-	return (
-		<div>LoginPage</div>
-	)
-}
+	const [isRightActive, setIsRightActive] = useState(false);
 
-export default LoginPage
+	return (
+		<>
+			<h2>Some text</h2>
+			<div
+				className={`container ${isRightActive ? "right-panel-active" : ""}`}
+				id="container"
+			>
+				<div className="form-container sign-up-container">
+					<form action="#">
+						<h1>Create Your SyncStream Account</h1>
+						<div className="social-container">
+							<img src={FacebookIcon} height={20} width={20} />
+							<img src={GoogleIcon} height={20} width={20} />
+						</div>
+						<span>Join and stream videos together in real-time</span>
+						<input type="text" placeholder="Name" />
+						<input type="email" placeholder="Email" />
+						<input type="password" placeholder="Password" />
+						<button>Sign Up</button>
+					</form>
+				</div>
+
+				<div className="form-container sign-in-container">
+					<form action="#">
+						<h1>Sign in</h1>
+						<div className="social-container">
+							<img src={FacebookIcon} height={20} width={20} />
+							<img src={GoogleIcon} height={20} width={20} />
+						</div>
+						<span>Watch and sync videos with friends</span>
+						<input type="email" placeholder="Email" />
+						<input type="password" placeholder="Password" />
+						<a href="#">Forgot your password?</a>
+						<button>Sign In</button>
+					</form>
+				</div>
+
+				<div className="overlay-container">
+					<div className="overlay">
+						<div className="overlay-panel overlay-left">
+							<h1>Welcome Back!</h1>
+							<p>Sign in to continue syncing videos with your friends and groups.</p>
+							<button
+								className="ghost"
+								id="signIn"
+								onClick={() => setIsRightActive(false)}
+							>
+								Sign In
+							</button>
+						</div>
+
+						<div className="overlay-panel overlay-right">
+							<h1>New to SyncStream?</h1>
+							<p>Create an account and start enjoying synchronized video streaming.</p>
+							<button
+								className="ghost"
+								id="signUp"
+								onClick={() => setIsRightActive(true)}
+							>
+								Sign Up
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default LoginPage;
