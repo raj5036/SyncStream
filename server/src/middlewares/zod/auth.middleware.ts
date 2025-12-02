f148df9e099f6830cb42e51f7caa0ed126983227
Raj Karmakar <97rajmath@gmail.com>
2025-12-02 11:36:22 +0530

    feat: Add Zod for request schema validation

---
+++ server/src/middlewares/zod/auth.middleware.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+import { NextFunction, Request, Response } from 'express';
+
+export const signupSchema = z.object({
+	email: z.string().email(),
+	password: z.string().min(6),
+	name: z.string().optional(),
+});
+
+export const loginSchema = z.object({
+	email: z.string().email(),
+	password: z.string().min(6),
+});
+
+export function validate(schema: any) {
+	return (req: Request, res: Response, next: NextFunction) => {
+		const result = schema.safeParse(req.body);
+		if (!result.success) {
+			return res.status(400).json({
+				errors: result.error.flatten().fieldErrors,
+			});
+		}
+		req.body = result.data; // cleaned + typed
+		next();
+	};
+}
--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,15 +1,18 @@
 import { Router } from "express";
 import * as AuthController from "../controllers/auth.controller";
+import { loginSchema, signupSchema, validate } from "../middlewares/zod/auth.middleware";
 
 const AuthRouter = Router();
 
 AuthRouter.post(
 	"/login",
+	validate(loginSchema),
 	AuthController.login
 );
 
 AuthRouter.post(
 	"/signup",
+	validate(signupSchema),
 	AuthController.signup
 );
 
